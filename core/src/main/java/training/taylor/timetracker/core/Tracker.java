cf0c01444386ff9a54c18b782dd01b4af0d000cb
yj6329 <49223100+yj6329@users.noreply.github.com>
2019-04-19 15:55:38 +0900

    change
    change

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,7 +16,7 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
-        if (true) {
+        if (true)  {
             entries.remove(entry);
         }
         entries.remove(entry);
